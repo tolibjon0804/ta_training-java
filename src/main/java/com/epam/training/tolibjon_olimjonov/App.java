49ccf4d8f0cf9108bb469e0f9a9a778daccb9b36
Tolibjon Olimjonov <tolibjon0804@gmail.com>
2023-02-12 16:15:05 +0500

    Initial commit

---
+++ src/main/java/com/epam/training/tolibjon_olimjonov/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.tolibjon_olimjonov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
